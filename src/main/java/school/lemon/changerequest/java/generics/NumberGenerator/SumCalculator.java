a2c43ae711acbbe979e3388f1073a2009fac28de
Lera Kulikova <eklera0506@gmail.com>
2017-01-07 11:18:57 +0200

    NumberGenerator has been fixed

--- src/main/java/school/lemon/changerequest/java/generics/NumberGenerator/SumCalculator.java
+++ src/main/java/school/lemon/changerequest/java/generics/NumberGenerator/SumCalculator.java
@@ -3,12 +3,9 @@ package school.lemon.changerequest.java.generics.NumberGenerator;
 /**
  * Created by lera on 07.01.17.
  */
-public class SumCalculator<T extends Number> {
+public abstract class SumCalculator<T extends Number> {
 
-    public T calculateSum(T[] array) {
-        T sum = (T) new Object();
-        return sum;
-    }
+    public abstract T calculateSum(T[] array);
 }
 
 
